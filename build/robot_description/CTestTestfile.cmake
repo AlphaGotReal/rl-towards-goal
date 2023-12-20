93f4d1036231bf69956ba53d0e060db617fc2210
Subham Patra <70360374+AlphaGotReal@users.noreply.github.com>
2023-12-20 20:30:42 +0530

    Delete build directory

--- build/robot_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/straw/Documents/Spice/white/src/robot_description
-# Build directory: /home/straw/Documents/Spice/white/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
